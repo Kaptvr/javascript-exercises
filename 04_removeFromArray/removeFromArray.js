9d793612c27d95f73e38590b7d499d754629cb7c
Franek Sornat <62433571+Kaptvr@users.noreply.github.com>
2023-08-17 11:58:52 +0200

    Finish js 04 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function() {
+  
+  for (let i = 0; i < arguments.length - 1; i++) {
+    let j = 0;
+    while (arguments[0][j] != undefined) {
+      if (arguments[0][j] === arguments[i + 1]) {
+        arguments[0].splice(j, 1);
+        j--;
+      }
+      j++
+    }
+  }
 
+  return arguments[0];
 };
 
 // Do not edit below this line
