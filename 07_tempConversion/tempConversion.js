61c09f9421d5e11fde7a8bc1003460a7cd3c86d3
Franek Sornat <62433571+Kaptvr@users.noreply.github.com>
2023-08-17 13:05:51 +0200

    Finish js 07 exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * (5 / 9);
+  if (Number.isInteger(celsius)) return celsius;
+  else {
+    return Number.parseFloat(celsius.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * 9 / 5) + 32;
+  if (Number.isInteger(fahrenheit)) return fahrenheit;
+  else {
+    return Number.parseFloat(fahrenheit.toFixed(1));
+  }
 };
+
+
 
 // Do not edit below this line
 module.exports = {
