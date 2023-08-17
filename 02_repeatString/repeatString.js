356f5c77425f12352c84a787aecaa3e2cc3139e7
Franek Sornat <62433571+Kaptvr@users.noreply.github.com>
2023-08-17 07:57:27 +0200

    Finish first 3 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(input, number) {
 
+  if (number < 0) {
+    return "ERROR"
+  }
+
+  if (number == 0) {
+    return ''
+  }
+
+  let string = input;
+
+  for (let i = 0; i < number - 1; i++) {
+    string += input;
+  }
+
+  return string;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+  let reverse = '';
+
+  for (let i = 0, j = string.length - 1; i < string.length; i++, j--) {
+    reverse += string[j];
+  }
+
+  return reverse;
 };
 
 // Do not edit below this line
