e86d8945d18843da98dffe4f386fe4d47d908e23
Franek Sornat <62433571+Kaptvr@users.noreply.github.com>
2023-08-17 12:37:10 +0200

    Finish js exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  
+  if (start < 0 || end < 0) {
+    return "ERROR";
+  }
 
-};
+  if (typeof start != "number" || typeof end != "number") {
+    return "ERROR";
+  }
+  
+  let sum = 0;
+
+  if (start > end) {
+    let temp = end;
+    end = start;
+    start = temp;
+  }
+
+  for (let i = start; i <= end; i++)
+  {
+    sum += i;
+  };
+
+  return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
